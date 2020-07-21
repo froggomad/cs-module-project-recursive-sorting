9b01e39ccd1a95ef95a0821c0b6e6c9dd89933c2
froggomad <kenny.dubroff@kennydubroff.com>
2020-07-21 16:04:35 -0700

    Working on merge sort

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,3 +1,4 @@
+import math
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
     #I don't understand the point of overcomplicating the operation by making an array of 0's    
@@ -7,28 +8,44 @@ def merge(arrA, arrB):
     right_count = len(arrB)
     left_i = 0
     right_i = 0
-    #
+    # fill the merged_arr with sorted values, checking both arrays until one of them has been
+    # completely processed
     while left_i < left_count and right_i < right_count:
         if arrA[left_i] < arrB[right_i]:
             merged_arr.append(arrA[left_i])
+            print(f"left: {arrA[left_i]}")
+            left_i += 1            
+        elif arrA[left_i] == arrB[right_i]:
             left_i += 1
-        else:
-            merged_arr.append(arrB[right_i])
             right_i += 1
-    # fill any remaining elements since the above loop will end when either array is processed - then return
-    merged_arr += arrA[left_i:]
-    merged_arr += arrB[right_i:]
+            merged_arr.append(arrA[left_i])
+            merged_arr.append(arrB[right_i])
+            print(f"both: {arrB[right_i]}")
+        else:
+            print(f"right: {arrB[right_i]}")
+            merged_arr.append(arrB[right_i])
+            right_i += 1            
+    # fill any remaining elements since the above loop will end when either array is processed - then return the result
+    if left_i != left_count:
+        merged_arr += arrA[left_i:]
+    if right_i != right_count:
+        merged_arr += arrB[right_i:]    
     return merged_arr
-
-print(merge([0,3,5], [1,2,4]))
-
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
-    # Your code here
-
-
+    # base case
+    if len(arr) <= 1:
+        return arr
+    #find the midpoint and split
+    mid = len(arr)//2
+    left = arr[:mid]
+    right = arr[mid:]
+    #recursively sort and merge the result
+    merge_sort(left)
+    merge_sort(right)
     return merge(left, right)
-
+arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+print(f"final: {merge_sort(arr1)}")
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
@@ -41,7 +58,7 @@ def merge_sort_in_place(arr, l, r):
     # Your code here
     pass
 
-import math
+
 
 
 """test code"""
